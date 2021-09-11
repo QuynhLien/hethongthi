b8bafe1455daa1900872c73b2bfc8f66ab4be5c3
QuynhLien97 <lien.vxq@aris-vn.com>
2021-09-11 20:06:38 +0700

    ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,6 +31,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \URL::forceScheme('https');
+
         view()->composer('*',function($view) {
             $token = Cookie::get('_token_mainte');
             if(!empty($token)){
