8bc4bc320c3abd5ba264219fed4586db37e4f0a1
QuynhLien97 <lien.vxq@aris-vn.com>
2021-09-11 20:41:32 +0700

    ssl

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://wellcomejp.herokuapp.com/public'),
+    'url' => env('APP_URL', 'https://wellcomejp.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
